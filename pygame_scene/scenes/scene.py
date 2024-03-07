78b634a33c061f911418477bc065c260c2209e53
EricZhou <ericzhou1101@gmail.com>
2024-03-07 14:35:09 +0000

    decoupled player

--- pygame_scene/scenes/scene.py
+++ pygame_scene/scenes/scene.py
@@ -129,17 +129,6 @@ class GameScene(Scene):
         self.notation_board = self._updated_display_board(self)
         
         self.current_turn = [BitBoard.colour.WHITE]
-        
-        self.evaluation_component = EvaluationComponent(self)
-        """
-        Values used in subroutines
-        """
-        
-        self.DRAG_DELAY = 0.5
-        self._mouse_held_position = None
-        self._drag_start_time = None
-        self._selected_tile = None
-        
         """
         GUI colours for different features of the scene will be moved to json file later
         """
@@ -188,96 +177,31 @@ class GameScene(Scene):
             if type(observer) is GameObserver:
                 observer.update_board_signal(self)
 
-
-#create player movement component that holds the functions and methods that involve player movement
+    def make_move(self, move:tuple=None):
+        self.update_board(move)
 
 class PlayerVsPlayer(GameScene):
     def __init__(self, width, height, bitboard=None):
         super().__init__(width, height, bitboard)
-    
-    def while_event(self, event):
-        mouse_to_vector = lambda mouse_x, mouse_y : Vector(mouse_x, mouse_y) // (self.dimensions // 8)
-        def make_move_if_legal(to_vector : Vector):
-            from_index = (7 - self._selected_tile.y) * 8 + (self._selected_tile.x)
-            to_index = (7 - to_vector.y) * 8 + (to_vector.x)
-            self._selected_tile = None
-            try: 
-                move_piece, move_colour = self.bitboard.index_to_piece_key(from_index)
-            except:
-                return
-            move = ((move_piece, move_colour), from_index, to_index)
-            
-            if move[1] in self._legal_moves.keys():
-                if move[2] in self._legal_moves[move[1]]:
-                    self.make_move(move)
-            
-        def click_event():
-            match event.button:
-                case 1: 
-                    mouse_x, mouse_y = pygame.mouse.get_pos()
-                    self._mouse_held_position = Vector(mouse_x, mouse_y)
-                    if self.vector_in_local_area(Vector(mouse_x, mouse_y)):
-                        if self._selected_tile:
-                            make_move_if_legal(mouse_to_vector(mouse_x, mouse_y))
-                        else:
-                            self._selected_tile = Vector(mouse_x, mouse_y) // (self.dimensions // 8)
-                            self._drag_start_time = time.time()
-                case 3:
-                    self._selected_tile = None
-                    
-        def release_event():
-            match event.button:
-                case 1:
-                    self._mouse_held_position = None
-                    mouse_x, mouse_y = pygame.mouse.get_pos()
-                    if self.vector_in_local_area(Vector(mouse_x, mouse_y)):
-                        if (time.time() - self._drag_start_time) > self.DRAG_DELAY:
-                            if self._selected_tile:
-                                make_move_if_legal(mouse_to_vector(mouse_x, mouse_y))
-                                
-        def mouse_motion_event():
-            if self._selected_tile and self._mouse_held_position:
-                self._mouse_held_position.x, self._mouse_held_position.y = event.pos
+        self.evaluation_component = EvaluationComponent(self)
+        self.player_componenet = PlayerComponent(self)
         
+    def while_event(self, event):
         match event.type:
             case pygame.VIDEORESIZE:
                 self.resize(event.h, event.w)
             case pygame.MOUSEBUTTONDOWN:
-                click_event()
+                self.player_componenet.click_event(event, self._legal_moves)
             case pygame.MOUSEBUTTONUP:
-                release_event()
+                self.player_componenet.release_event(event, self._legal_moves)
             case pygame.MOUSEMOTION:
-                mouse_motion_event()
-    
+                self.player_componenet.mouse_motion_event(event)
+                
     def draw(self, window):
-        tilesize = self.dimensions // 8
-        if self._selected_tile and self.vector_to_index(self._selected_tile) in self._legal_moves.keys():
-            legal_move_vectors = [self.index_to_vector(index) if type(index) is int else self.index_to_vector(index[0]) for index in self._legal_moves[self.vector_to_index(self._selected_tile)]]
-        else: legal_move_vectors = []
-        
-        selected_piece_drag_position = selected_notation = None
-        for r_index, row in enumerate(self.notation_board):
-            for c_index, notation in enumerate(row):
-                
-                tile_rect = pygame.Rect(c_index * tilesize.x, r_index * tilesize.y, tilesize.x, tilesize.y)
-                if (c_index, r_index) in [(vector.x, vector.y) for vector in legal_move_vectors]:
-                    pygame.draw.rect(window, self._possible_move_colour, tile_rect)
-                elif (c_index + r_index) % 2 == 0: pygame.draw.rect(window, self._light_tile_colour, tile_rect)
-                else: pygame.draw.rect(window, self._dark_tile_colour, tile_rect)
-                
-                if self._selected_tile and (c_index, r_index) == (self._selected_tile.x, self._selected_tile.y):
-                    pygame.draw.rect(window, self._select_colour, tile_rect)
-                    if notation != ".":
-                        if self._mouse_held_position and self.within_board(self._mouse_held_position):
-                            selected_piece_drag_position = (self._mouse_held_position.x - ((self.dimensions.x // 8) // 2), self._mouse_held_position.y - ((self.dimensions.y // 8) // 2))
-                            selected_notation = notation
-                        else:
-                            window.blit(self.notation_to_image[notation], (c_index * (self.dimensions.x // 8), r_index * (self.dimensions.y // 8)))
-                elif notation != ".":
-                    window.blit(self.notation_to_image[notation], (c_index * (self.dimensions.x // 8), r_index * (self.dimensions.y // 8)))
-                    
-        if selected_notation and selected_piece_drag_position:
-            window.blit(self.notation_to_image[selected_notation], selected_piece_drag_position)
+        if self.player_componenet.selected_tile:
+            self.player_componenet.draw_board_with_selection(window, self._legal_moves, self._possible_move_colour, self._select_colour, self._light_tile_colour, self._dark_tile_colour)
+            return
+        super().draw(window)
     
     def make_move(self, move):
         self.switch_colour(self.current_turn)
@@ -287,21 +211,104 @@ class PlayerVsPlayer(GameScene):
 class PlayerVsComputer(GameScene):
     def __init__(self, width, height, bitboard=None):
         super().__init__(width, height, bitboard)
+        self.evaluation_component = EvaluationComponent(self)
+        self.player_componenet = PlayerComponent(self, self.current_turn[0])
+        
     def while_event(self, event):
-        pass
+        match event.type:
+            case pygame.VIDEORESIZE:
+                self.resize(event.h, event.w)
+            case pygame.MOUSEBUTTONDOWN:
+                self.player_componenet.click_event(event, self._legal_moves)
+            case pygame.MOUSEBUTTONUP:
+                self.player_componenet.release_event(event, self._legal_moves)
+            case pygame.MOUSEMOTION:
+                self.player_componenet.mouse_motion_event(event)
+    
     def draw(self, window):
         pass
 
-
 class PlayerComponent():
-    def __init__(self, parent) -> None:
+    def __init__(self, parent : GameScene, turn_colour=None) -> None:
         self.parent = parent
+        self.turn_colour = turn_colour
+        self.mouse_to_vector = lambda mouse_x, mouse_y : Vector(mouse_x, mouse_y) // (self.parent.dimensions // 8)
         
         self.DRAG_DELAY = 0.5
-        self._mouse_held_position = None
-        self._drag_start_time = None
-    
-    def 
+        self.mouse_held_position = None
+        self.drag_start_time = None
+        self.selected_tile = None
+        
+    def make_move_if_legal(self, to_vector : Vector, legal_moves):
+        from_index = (7 - self.selected_tile.y) * 8 + (self.selected_tile.x)
+        to_index = (7 - to_vector.y) * 8 + (to_vector.x)
+        self.selected_tile = None
+        try:
+            move_piece, move_colour = self.parent.bitboard.index_to_piece_key(from_index)
+        except:
+            return
+        move = ((move_piece, move_colour), from_index, to_index)
+        
+        if move[1] in legal_moves.keys() and move[2] in legal_moves[move[1]] and (not(self.turn_colour) or self.parent.current_turn[0] == self.turn_colour):
+            self.parent.make_move(move)
+        
+    def click_event(self, event, legal_moves):
+        match event.button:
+            case 1: 
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                self.mouse_held_position = Vector(mouse_x, mouse_y)
+                if self.parent.vector_in_local_area(Vector(mouse_x, mouse_y)):
+                    if self.selected_tile:
+                        self.make_move_if_legal(self.mouse_to_vector(mouse_x, mouse_y), legal_moves)
+                    else:
+                        self.selected_tile = Vector(mouse_x, mouse_y) // (self.parent.dimensions // 8)
+                        self.drag_start_time = time.time()
+            case 3:
+                self.selected_tile = None
+                
+    def release_event(self, event, legal_moves):
+        match event.button:
+            case 1:
+                self.mouse_held_position = None
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                if self.parent.vector_in_local_area(Vector(mouse_x, mouse_y)):
+                    if (time.time() - self.drag_start_time) > self.DRAG_DELAY:
+                        if self.selected_tile:
+                            self.make_move_if_legal(self.mouse_to_vector(mouse_x, mouse_y), legal_moves)
+                            
+    def mouse_motion_event(self, event):
+        if self.selected_tile and self.mouse_held_position:
+            self.mouse_held_position.x, self.mouse_held_position.y = event.pos
+
+    def draw_board_with_selection(self, window, legal_moves, possible_move_colour, select_colour, light_tile_colour, dark_tile_colour):
+        tilesize = self.parent.dimensions // 8
+        if self.selected_tile and self.parent.vector_to_index(self.selected_tile) in legal_moves.keys():
+            legal_move_vectors = [self.parent.index_to_vector(index) if type(index) is int else self.parent.index_to_vector(index[0]) for index in legal_moves[self.parent.vector_to_index(self.selected_tile)]]
+        else: legal_move_vectors = []
+        
+        selected_piece_drag_position = selected_notation = None
+        for r_index, row in enumerate(self.parent.notation_board):
+            for c_index, notation in enumerate(row):
+                
+                tile_rect = pygame.Rect(c_index * tilesize.x, r_index * tilesize.y, tilesize.x, tilesize.y)
+                if (c_index, r_index) in [(vector.x, vector.y) for vector in legal_move_vectors]:
+                    pygame.draw.rect(window, possible_move_colour, tile_rect)
+                elif (c_index + r_index) % 2 == 0: pygame.draw.rect(window, light_tile_colour, tile_rect)
+                else: pygame.draw.rect(window, dark_tile_colour, tile_rect)
+                
+                if self.selected_tile and (c_index, r_index) == (self.selected_tile.x, self.selected_tile.y):
+                    pygame.draw.rect(window, select_colour, tile_rect)
+                    if notation != ".":
+                        if self.mouse_held_position and self.parent.within_board(self.mouse_held_position):
+                            selected_piece_drag_position = (self.mouse_held_position.x - ((self.parent.dimensions.x // 8) // 2), self.mouse_held_position.y - ((self.parent.dimensions.y // 8) // 2))
+                            selected_notation = notation
+                        else:
+                            window.blit(self.parent.notation_to_image[notation], (c_index * (self.parent.dimensions.x // 8), r_index * (self.parent.dimensions.y // 8)))
+                elif notation != ".":
+                    window.blit(self.parent.notation_to_image[notation], (c_index * (self.parent.dimensions.x // 8), r_index * (self.parent.dimensions.y // 8)))
+                    
+        if selected_notation and selected_piece_drag_position:
+            window.blit(self.parent.notation_to_image[selected_notation], selected_piece_drag_position)
 
 class EvaluationComponent():
     def __init__(self, parent : GameScene) -> None:
@@ -318,7 +325,6 @@ class EvaluationComponent():
             return
         self.__update_move = move
         self.__evaluation_threads[-1].engine.max_time = 0
-        print(self.__evaluation_threads[-1].engine.max_time)
     
     def create_new_thread(self, move_evaluation):
         move_evaluation = move_evaluation[self.__update_move] if move_evaluation and self.__update_move in move_evaluation.keys() \
